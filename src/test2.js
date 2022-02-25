dcd7f0290450c1ddff58f036f79fd8f8767825f6
naamanjagga <namanjagga@cedcommerce.com>
2022-02-25 18:21:12 +0530

    color picker

---
+++ src/test2.js
@@ -0,0 +1,18 @@
+$(document).ready( function(){
+    $('#black').click( function() {
+        $('#change_color').css('background-color','black')
+    })
+    $('#red').click( function() {
+        $('#change_color').css('background-color','red')
+    })
+    $('#blue').click( function() {
+    
+        $('#change_color').css('background-color','blue')
+    })
+    $('#green').click( function() {
+        $('#change_color').css('background-color','green')
+    })
+    $('#yellow').click( function() {
+        $('#change_color').css('background-color','yellow')
+    })
+});
